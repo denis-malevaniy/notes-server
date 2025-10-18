08262a7db18d35f506d3afa9efcd6af3e26c0304
malevaniy-denis <malevaniydenis@gmail.com>
2025-10-18 15:51:00 +0300

    add func: add funcs addNote notesHandler and fix func main

--- cmd/notes-server/main.go
+++ cmd/notes-server/main.go
@@ -1,15 +1,45 @@
 package main
 
+//заметки хранятся в ОП(потом заменить на БД)
+
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
-	})
+type Note struct {
+    ID      int    `json:"id"`
+    Title   string `json:"title"`
+    Content string `json:"content"`
+}
 
-	fmt.Println("Server started at http://localhost:9000")
+var (
+	notes []Note
+	mu    sync.Mutex
+	nextID = 1
+)
+
+func addNote(title, content string){
+	mu.Lock()
+	defer mu.Unlock()
+	note := Note{ID: nextID, Title: title, Content: content}
+	nextID++ 
+	notes = append(notes, note)
+}
+
+func notesHandler (w http.ResponseWriter, r * http.Request){
+	mu.Lock()
+	defer mu.Unlock()
+	w.Header().Set("Content-Type", "application/json")
+	json,json.NewEncoder(w).Encode(notes)
+}
+
+func main() {
+	addNote("FIRST", "UOUOUOUOUOUUOOU GO GO")
+
+	http.HandleFunc("/notes", notesHandler)
+	fmt.Println("started at http://localhost:9000")
 	http.ListenAndServe(":9000", nil)
 }
